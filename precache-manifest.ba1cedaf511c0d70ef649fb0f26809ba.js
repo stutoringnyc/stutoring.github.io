5e8cc39e94d97bfaa96b12b278042b9f2770e1d2
JiaqiGao <jiaqigao12@gmail.com>
2023-04-15 16:29:33 -0400

    Updates

---
+++ precache-manifest.ba1cedaf511c0d70ef649fb0f26809ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "477290a5f0d581a2d354",
+    "url": "/static/css/main.2a5f3f17.chunk.css"
+  },
+  {
+    "revision": "477290a5f0d581a2d354",
+    "url": "/static/js/main.477290a5.chunk.js"
+  },
+  {
+    "revision": "3e3c3eb55c09e01a1300",
+    "url": "/static/js/1.3e3c3eb5.chunk.js"
+  },
+  {
+    "revision": "ff813772de7efb309245",
+    "url": "/static/js/2.ff813772.chunk.js"
+  },
+  {
+    "revision": "bd5440dfbcfe3e971529",
+    "url": "/static/js/runtime~main.bd5440df.js"
+  },
+  {
+    "revision": "ed51a2c444c77628c6aa41e0d565dff3",
+    "url": "/index.html"
+  }
+];
